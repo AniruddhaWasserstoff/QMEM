86c455d2bc96a869bb59dad17ac870c21a0039dc
AniruddhaBolakhe <aniruddhabolakhe2907@gmail.com>
2025-09-03 15:20:21 +0530

    first commit

---
+++ test_qmem.py
@@ -0,0 +1,15 @@
+import qmem as qm
+
+# Create a collection
+qm.create(collection_name="qwerty", dim=1536, distance_metric="cosine")
+
+# Ingest data from a file
+qm.ingest(
+    file="/home/aniruddha/Desktop/qmem/data.jsonl",
+    embed_field="response",
+    payload_field="query,response",  # optional, keep these fields in payload
+)
+
+# Retrieve results (pretty table by default)
+table = qm.retrieve(query="list customers", top_k=5)
+print(table)
